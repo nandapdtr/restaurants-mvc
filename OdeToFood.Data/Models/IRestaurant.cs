fbe33fd1b7bc805e97eddafb84dd76fb9feb3653
nrabba <nrabba@DESKTOP-15RDSPA>
2022-03-12 17:51:38 +0100

    Add project files.

---
+++ OdeToFood.Data/Models/IRestaurant.cs
@@ -0,0 +1,10 @@
+﻿namespace OdeToFood.Data.Models
+{
+    public interface IRestaurant
+    {
+        int Id { get; }
+        string Name { get; }
+        CuisineType Cuisine { get; }
+
+    }
+}
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id = 1, Name = "Bawurchi", Cuisine = CuisineType.Indian},
+                new Restaurant{Id = 2, Name = "Pizzahut", Cuisine = CuisineType.Italian},
+                new Restaurant{Id = 3, Name = "Talpakatti Biryani", Cuisine = CuisineType.French},
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.Find(x => x.Id == id);
+        }
+    }
+}
