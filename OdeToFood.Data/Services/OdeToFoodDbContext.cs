6ea8a54ba86f4620559fe87b78bc2cc529b66675
nandapdtr <nandapdtr@gmail.com>
2022-03-13 13:20:59 +0100

    Added DbContext for Restaurants

---
+++ OdeToFood.Data/Services/OdeToFoodDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class OdeToFoodDbContext: DbContext
+    {
+        //This creates a table called Restaurants in the database and creates the columns based on the model Restaurant
+        //If the model contains Id property then entity framework makes it as a primary key and treat it as an identifier
+        //The database choosen by entity framework will be based on the connection string in the App.config file
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ OdeToFood.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Restaurant restaurant)
+        {
+            //This step prepares the data to be added to the table
+            db.Restaurants.Add(restaurant);
+            //This step will add the data into the table
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return db.Restaurants;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            //This approach we should not do because if the different users updating the same record then the last user update will be taken
+            //var r = Get(restaurant.Id);
+            //r.Name = restaurant.Name;
+            //db.SaveChanges();
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
